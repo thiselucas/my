88c12f8dc56efb8e349ac84f725ca8f873953645
lucasescotodaluz <lucas.escoto@acad.pucrs.br>
2023-10-02 17:18:16 -0300

    inicio project.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { WorksComponent } from './works/works.component';
 import { EmailComponent } from './email/email.component';
 import { HomeComponent } from './home/home.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
 
 
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, title: 'Lucas Escoto da Luz'}
+  {path: '', component: PersonalInformationComponent, title: 'Lucas Escoto da Luz'}
 ];
 
 @NgModule({
